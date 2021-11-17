3034d13ad3a3581e3865fef6317ca735b2e3c5cb
S3ttez <ebuc2000@gmail.com>
2021-11-17 17:02:04 +0100

    Add Price&Quantity test

---
+++ test/ItemsTesting.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Alex on 17/11/2021.
+//
+
+#include "gtest/gtest.h"
+#include "../Items.h"
+#include "../Items.cpp"
+
+class ItemsTesting : public ::testing::Test {
+public:
+    void SetUp() override {
+        q.setQuantity(0);
+        p.setPrice(0);
+    }
+    void TearDown() override {}
+    Items q;
+    Items p;
+};
+
+TEST_F(ItemsTesting, QuantityTestingMethod){
+    ASSERT_THROW({
+        try{
+            q.setQuantity(-5);
+        }
+        catch(const invalid_argument& e){
+            ASSERT_STREQ("Error input. Change!", e.what());
+            throw;
+        }
+        },invalid_argument);
+}
+
+TEST_F(ItemsTesting, PriceTestingMethod){
+    ASSERT_THROW({
+        try{
+            p.setPrice(-5);
+        }
+        catch(const invalid_argument& e){
+            ASSERT_STREQ("Error input. Change!", e.what());
+            throw;
+        }
+        },invalid_argument);
+}
